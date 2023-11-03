245d2945724e205d28ce38c575a109c67fd89c2d
Emma Gerigscott <elgerig@gmail.com>
2023-11-03 09:12:34 -0700

    basic logic for minute and hour + test 2 passing

--- clock.js
+++ clock.js
@@ -1,10 +1,28 @@
 export class Clock {
-  constructor(hour) {
+  constructor(hour = 0, minute = 0) {
     this.hour = hour
+    this.minute = minute
   }
 
   toString() {
-    return `0${this.hour}:00`
+    let hourString = '';
+    if (this.hour < 10) {
+      hourString = `0${this.hour}`
+    } else if (this.hour > 10 && this.hour < 24) {
+      hourString = `${this.hour}`
+    } else if (this.hour === 24) {
+      hourString = '00'
+    }
+
+    let minuteString = '';
+    if (this.minute < 10) {
+      minuteString = `0${this.minute}`
+    } else if (this.minute > 10 && this.minute < 60) {
+      minuteString = `${this.minute}`
+    } else if (this.minute === 60) {
+      minuteString = '00'
+    }
+    return `${hourString}:${minuteString}`
   }
 
   plus() {
